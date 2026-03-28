b763bad5d053dc439df23fc375723b91671fab08
chandrakant-verma <vermachandra896@gmail.com>
2026-03-28 09:10:39 +0530

    added ticket model

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config( {path:"./.env"} );
+app.use(express.static("public"));
+
+
+app.use(cors({  origin: process.env.CORS_ORIGIN,credentials : true  }))
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+
+const connectDB = async()=>{
+       
+    try{
+         const connectionInstance = await mongoose.connect( `${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+         console.log(`mongoDB connected connection Host : ${connectionInstance.connection.host} `);
+    }catch(error){
+        console.log("monogoDB connection failed with error : ", error);
+        process.exit(1);
+    }
+}
+
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT}`);
+  });
+}).catch((error) => {
+  console.error("Database connection failed:", error);
+});
---
+++ src/model/tickets.js
@@ -0,0 +1,51 @@
+import mongoose, {Schema} from "mongoose";
+
+const ticketSchema = new Schema(
+    {
+        customername: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
+        },
+        productname:{
+             type: String,
+             required: true
+        },
+
+        issue:{
+            type: String,
+            required: true
+        },
+  
+        category: {
+             type: String,
+             required: true,
+             enum: ["delivery delay", "replacement delay", "damaged product", "wrong item", "product quality",  "refund"]
+        },
+
+        status:{
+             type: String,
+             required: true,
+             enum: ["open", "closed","resolved"]
+        },
+
+        priority:{
+             type: String,
+             required: true,
+             enum: ["hight", "medium", "low"]
+        },
+
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Ticket = mongoose.model("Ticket", ticketSchema)
